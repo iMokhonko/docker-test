0716a444d6c271b796d9cb3dc10bbb3b9d05cb28
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2022-03-09 19:56:00 +0200

    chore: implement connect with retry for db connection & add depends_on to docker-compose

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const app = require('express')();
 
-const app = express();
 const mongoose = require('mongoose');
+
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
-mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`)
-  .then(() => { console.log('connected to DB'); })
-  .catch((e) => { console.error(e); })
+const connectWithRetry = () => {
+  const mongoConnectUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`;
+  
+  mongoose.connect(mongoConnectUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+    .then(() => { console.log('connected to DB'); })
+    .catch((e) => { 
+      console.error(e); 
+      setTimeout(connectWithRetry, 5000);
+    })
+}
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
   res.send(`mode: ${process.env.NODE_ENV}`);
