5f064f89cac1fab9c4860516a6d3a15f11fe729e
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2022-03-09 19:44:08 +0200

    chore(mongo): add mongo db & connect to db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`)
+  .then(() => { console.log('connected to DB'); })
+  .catch((e) => { console.error(e); })
 
 app.get('/', (req, res) => {
   res.send(`mode: ${process.env.NODE_ENV}`);
