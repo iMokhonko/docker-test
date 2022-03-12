9172dd9acb238885d639bd6582bd12c58fa5279e
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2022-03-12 13:43:15 +0200

    chore: kek

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const cors = require('cors');
 app.enable('trust proxy');
 app.use(cors());
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const connectWithRetry = () => {
   const mongoConnectUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`;
+
+  console.error('mongoConnectUrl', mongoConnectUrl);
   
-  mongoose.connect(mongoConnectUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => { console.log('connected to DB'); })
-    .catch((e) => { 
-      console.error(e); 
-      setTimeout(connectWithRetry, 5000);
-    })
+  // mongoose.connect(mongoConnectUrl, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // })
+  //   .then(() => { console.log('connected to DB'); })
+  //   .catch((e) => { 
+  //     console.error(e); 
+  //     setTimeout(connectWithRetry, 5000);
+  //   })
 }
 
 connectWithRetry();
