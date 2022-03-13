d43d92ac6080c861e9885c43bcefd882bfb858e2
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2022-03-13 18:23:14 +0200

    chore(ci): change deploy script

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ connectWithRetry();
 
 app.get('/', (req, res) => {
   console.log(req.headers);
-  res.send(`mode: ${process.env.NODE_ENV} (DEV env)`);
+  res.send(`mode: ${process.env.NODE_ENV} (DEV env with swarm)`);
 })
 
 const port = process.env.PORT || 3000;
